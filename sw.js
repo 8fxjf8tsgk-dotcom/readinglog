3fe7c62f9d78a650c6140546569e92e9b0bb643f
8fxjf8tsgk-dotcom <8fxjf8tsgk@privaterelay.appleid.com>
2026-01-31 02:24:24 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+// Simple offline cache for static assets
+const CACHE = "readinglog-pwa-v2-20260131";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : Promise.resolve())))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+  event.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(req, copy)).catch(()=>{});
+      return res;
+    }).catch(()=>cached))
+  );
+});
